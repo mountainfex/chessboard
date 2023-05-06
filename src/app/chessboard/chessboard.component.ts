acc8cccc2de47dcf9ff5d2ea18a5c15136cd850b
Niklas <niklas.tibbe@gmail.com>
2023-05-06 21:54:43 +0200

    feat: added calculatePossibleMoves

--- src/app/chessboard/chessboard.component.ts
+++ src/app/chessboard/chessboard.component.ts
@@ -6,6 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./chessboard.component.scss'],
 })
 export class ChessboardComponent {
-  column = [1, 2, 3, 4, 5, 6, 7, 8];
-  row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  row = [1, 2, 3, 4, 5, 6, 7, 8];
+  column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+  newRow: number | null = null;
+  newCol: number | null = null;
+
+  showPossibleMoves(possibleRow: number, possibleColumn: string) {
+    // alert(possibleRow + possibleColumn);
+    this.calculatePossibleMoves(
+      possibleRow,
+      this.column.indexOf(possibleColumn)
+    );
+  }
+
+  calculatePossibleMoves(row: number, col: number) {
+    const possibleMoves = [];
+    const directions = [
+      [-2, 1],
+      [-1, 2],
+      [1, 2],
+      [2, 1],
+      [2, -1],
+      [1, -2],
+      [-1, -2],
+      [-2, -1],
+    ];
+    for (const direction of directions) {
+      this.newRow = row + direction[0];
+      this.newCol = col + direction[1];
+      console.log(this.newRow, this.column[this.newCol]);
+    }
+  }
 }
