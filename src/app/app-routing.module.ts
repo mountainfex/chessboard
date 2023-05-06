5646997c66f48b48814fd309ddf507a08ad35019
Niklas <niklas.tibbe@gmail.com>
2023-05-06 23:27:31 +0200

    feat: added function to highlight possible moves

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChessboardComponent } from './chessboard/chessboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'start=:id', component: ChessboardComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/chessboard/chessboard.component.ts
+++ src/app/chessboard/chessboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-chessboard',
@@ -6,22 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./chessboard.component.scss'],
 })
 export class ChessboardComponent {
-  row = [1, 2, 3, 4, 5, 6, 7, 8];
-  column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  row: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+  column: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  possibleMoves: any[] = [];
+  previousEvent: any = null;
 
   newRow: number | null = null;
   newCol: number | null = null;
 
-  showPossibleMoves(possibleRow: number, possibleColumn: string) {
-    // alert(possibleRow + possibleColumn);
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+
+  showPossibleMoves(
+    possibleRow: number,
+    possibleColumn: string,
+    event: Event,
+    i: number,
+    j: number
+  ) {
+    this.activatedRoute.paramMap.pipe();
+    this.router.navigate(['/start=' + this.column[i] + this.row[j]]);
+
+    this.previousEvent?.classList.remove('start');
+    this.previousEvent = event.target as HTMLElement;
+    this.previousEvent.classList.add('start');
+
     this.calculatePossibleMoves(
-      possibleRow,
+      this.row.indexOf(possibleRow),
       this.column.indexOf(possibleColumn)
     );
   }
 
   calculatePossibleMoves(row: number, col: number) {
-    const possibleMoves = [];
+    this.possibleMoves = [];
     const directions = [
       [-2, 1],
       [-1, 2],
@@ -35,7 +52,16 @@ export class ChessboardComponent {
     for (const direction of directions) {
       this.newRow = row + direction[0];
       this.newCol = col + direction[1];
-      console.log(this.newRow, this.column[this.newCol]);
+      this.possibleMoves.push(this.column[this.newCol] + this.row[this.newRow]);
+      // console.log(this.row[this.newRow] + this.column[this.newCol]);
+    }
+  }
+
+  checkIfPossible(i: number, j: number) {
+    if (this.possibleMoves.includes(this.column[i] + this.row[j])) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
